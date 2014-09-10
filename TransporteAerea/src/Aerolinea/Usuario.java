2d752c8e21269f0460dbf6caec926659647b770a
Bladimir Alejandro Grajeda Ayala <bladimir_alejandro12@hotmail.com>
2014-09-09 22:43:02 -0600

    Pruebas

--- TransporteAerea/src/Aerolinea/Usuario.java
+++ TransporteAerea/src/Aerolinea/Usuario.java
@@ -2,4 +2,35 @@ package Aerolinea;
 
 public class Usuario {
     
+    private String name,user,tip;
+    private int pass;
+   
+    
+    
+    //Constructor
+    public Usuario( String use,int pas, String tp, String nam){
+        name = nam;
+        user = use;
+        pass= pas;
+        tip = tp;
+        
+        
+    }
+    
+    
+    public void edit(String nam , String use, int pas , String tp){
+        name = nam;
+        user = use;
+        pass = pas;
+        tip = tp;
+    }
+    
+    public boolean creado(String u){
+        if(user == u )
+            return true;
+        return false;
+    }
+    
+    
+    
 }
