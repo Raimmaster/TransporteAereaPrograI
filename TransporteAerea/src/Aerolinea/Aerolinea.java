1dc84e4210d0e590317072cb580087493e614fde
Raim Interiano <rai.interiano@gmail.com>
2014-09-16 16:47:28 -0600

    Commit de Prueba - Alternativa a la Raim
    Commit para subir los archivos del programa según la versión de Raim. En
    este branch sólo se hacen pruebas; el proyecto original y completo debe
    ir en el branch master.

--- TransporteAerea/src/Aerolinea/Aerolinea.java
+++ TransporteAerea/src/Aerolinea/Aerolinea.java
@@ -3,112 +3,251 @@ package Aerolinea;
 import java.util.Scanner;
 
 public class Aerolinea {
-    int max, resp;
-    Ruta rutas[];
-    String asientos[][];
-    Scanner rd = new Scanner(System.in);
-    
-    public Aerolinea(){
-        System.out.print("Ingrese cantidad maxima de rutas de la Aerolinea: "); 
-        max = rd.nextInt();
-        rutas = new Ruta[max];
-        asientos = new String[max][];
-    }
-    
-    public void crearRuta(){
-        int vacia = rutaVacia();
-            if(vacia>=0){
-                rutas[vacia] = new Ruta(vacia);
-                asientos[vacia] = new String[rutas[vacia].getMaxiAsientos()];
-            }
-        System.out.print("\n\n");
-    }
-    
-    public void venderTicket(){
-        int num;
-        System.out.print("Ingrese numero de vuelo: "); num = rd.nextInt();
-        if (getFilaById(num)!=-1 && rutaDisponibleByFila(getFilaById(num))==true){
-            if (rutas[getFilaById(num)].addPasajeroToRuta()==true){
-                asientos[getFilaById(num)][Ruta.getAsientoCreado()] = Ruta.getIdCreado();
-            }
-        }else
-            System.out.print("Numero de vuelo no existe\n");
-        System.out.println("");
-    }
-    
-    public void cancelarTicket(){
-        int num;
-        System.out.print("Ingrese numero de vuelo: "); num = rd.nextInt();
-        if (getFilaById(num)!=-1 && rutaDisponibleByFila(getFilaById(num))==true)
-            rutas[getFilaById(num)].deletePasajero();
-        else
-            System.out.print("Numero de vuelo no existe\n");
-        System.out.println("");
-    }
-    
-    
+    //CONSTANTES
+    private int CANT_MAXIMA_RUTAS;
+    private static final int CANT_USERS = 10;
+    //OBJETOS
+    private Usuario [] users = new Usuario[CANT_USERS];
+    private Ruta [] rutas;
+    //OTRAS VARIABLES
+    private String [][] listadoAsientos;
+    private static Scanner lea = new Scanner(System.in);
+
     /**
-     * Funcion para ver si hay espacios para crear rutas
-     * @return Retorna la posicion del arreglo donde hay espacio para crear ruta
+     * Constructor de Aerolinea
+     * @param cantRutas Cantidad máxima de rutas que tendrá la aerolinea
      */
-    private int rutaVacia(){
-        //System.out.print("Entro en rutavacia");
-        for (int i = 0; i < max; i++) {
-            if (rutas[i]==null){
+    public Aerolinea(int cantRutas){
+        CANT_MAXIMA_RUTAS = cantRutas;
+        rutas = new Ruta[CANT_MAXIMA_RUTAS];
+        listadoAsientos = new String[CANT_MAXIMA_RUTAS][];
+        users[0] = new Usuario("admin", "admin", "admin", "ADMIN");        
+    }
+
+    /**
+     * Función para encontrar la próxima posición disponible
+     * en el arreglo de rutas
+     * @return Regresa la posición i del arreglo o -1 si el 
+     * arreglo ya está lleno
+     */
+    public int nextRutaPos(){
+        for (int i = 0; i < CANT_MAXIMA_RUTAS; i++){
+            if (rutas[i] == null)
                 return i;
-            }
         }
-        System.out.print("No hay rutas vacias");
-        return -1;
-    }
-    
-    private boolean rutasCreadas(){
-        for (int i = 0; i < max; i++) {
-            if (rutas[i]!=null){
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    /**
-     * 
-     * @param x Especifica la posicion del arreglo para verificar si existe
-     * @return Retorna true si existe la ruta o false en caso contrario
-     */
-    private boolean rutaDisponibleByFila(int x){
-        if (rutas[x]!=null){
-                System.out.printf("\tRuta: %d\n\tDestino: %s\n", rutas[x].getNumeroVuelo(), rutas[x].getDestino());
-                return true;
-        }
-        System.out.print("No existe dicha ruta\n\n");
-        return false;
-    }
-    
-    private int getFilaById(int x){
-        for (int i = 0; i < max; i++) {
-            if (rutas[i]!=null && rutas[i].getId()==x){
-                return rutas[i].getFila();
-            }
-        }
+
         return -1;
     }
     
     /**
-     * Funcion para devolver la ruta que mas ganancias percibio
-     * @return Retorna el objeto Ruta que percibio mas ganancias
+     * Funcion para buscar si la ruta ya existe
+     * @param nVuelo Numero de vuelo que se desea buscar
+     * @return Regresa el objeto Ruta encontrado si existe o null si no existe.
      */
-    private Ruta rentable(){
-        Ruta rent = null;
-        double gan = 0;
-        for (int i = 0; i < max; i++) {
-            if (rutas[i]!=null &&rutas[i].getGanancias()>gan){
-                gan = rutas[i].getGanancias();
-                rent = rutas[i];
+    public Ruta buscarRuta(int nVuelo){        
+        for (Ruta route : rutas){
+            if (route != null && route.getNumVuelo() == nVuelo){
+                System.out.printf("Ruta con el numero %d existe.\n", route.getNumVuelo());
+                return route;            
             }
         }
-        if (rent==null)
-            return null;
-        return rent;
+        System.out.printf("Ruta con el numero %d no existe\n", nVuelo);
+        return null;
+    }    
+
+    /**
+     * Funcion para autenticar el usuario
+     * @param us Username que se autenticara
+     * @param pass Password que se autenticara
+     * @return Usuario con las credenciales correctas
+     */
+    public Usuario autenticarUsuario(String us, String pass){
+        for (Usuario usuario : users){
+            if (usuario != null && usuario.getUsername().equals(us) && usuario.getPassword().equals(pass))
+                return usuario;
+        }
+
+        return null;
+    }
+
+    /**
+     * Funcion login para poder acceder al menu
+     * @return Usuario el usuario del login
+     */
+    public Usuario login(){
+        Usuario userActual;
+        do{
+            System.out.print("\n\n\t***LOGIN***\nIngrese su username: ");
+            String us = lea.next();
+            System.out.print("Ingrese su password: ");
+            String pass = lea.next();
+
+            userActual = autenticarUsuario(us, pass);
+            if (userActual != null)            
+                return userActual;  
+        
+            System.out.println("LOGIN INVALIDO! INGRESE SUS DATOS DE NUEVO.");
+        }while (userActual == null);
+
+        return null;
+    }
+
+    /**
+     * Función para encontrar la próxima posición en el arreglo de usuario.
+     * @return La posición i en el arreglo de usuarios
+     */
+    public int nextUserPos(){
+        for (int i = 0; i < CANT_USERS; i++){
+            if (users[i] == null)
+                return i;
+        }
+        return -1;
+    }
+    
+    /**
+     * Funcion para crear una ruta.
+     */
+    public void createRuta(){
+        int esp = nextRutaPos();        
+        if (esp == -1){
+            System.out.println("Las rutas totales han sido llenadas; ya no hay espacio.");
+            return;
+        }
+        int n = 0;
+        do{
+            System.out.print("\nIngrese el numero de vuelo: ");
+            n = lea.nextInt();
+        }while(buscarRuta(n) != null);
+        
+        System.out.print("\nIngrese la ciudad de destino: ");
+        String c = lea.next();
+        System.out.print("Ingrese la cantidad maxima de asientos que posee el vuelo: ");
+        int max = lea.nextInt();
+        listadoAsientos[esp] = new String[max];//asignar la cantidad maxima de asientos a la fila/vuelo
+        System.out.print("Ingrese el limite de asientos de primera clase: ");
+        int pClas = lea.nextInt();
+        System.out.print("Ingrese el precio de los asientos de primera clase: ");
+        double precP = lea.nextDouble();
+        System.out.print("Ingrese el precio de los asientos de clase economica: ");
+        double precE = lea.nextDouble();
+        System.out.print("Ingrese el costo que conlleva despachar esta ruta: ");
+        double cost = lea.nextDouble();
+        
+        rutas[esp] = new Ruta(n, c, max, pClas, precP, precE, cost, esp);  
+        
+    }
+    
+    /**
+     * Encuentra la cantidad de asientos disponibles en el vuelo
+     * @param pos La posicion del vuelo en el arreglo.
+     * @return La cantidad de asientos disponibles en el vuelo.
+     */
+    public int asientosDisponibles(int pos){       
+        int disponibles = 0;
+              
+        for (int i = 0; i < listadoAsientos[pos].length; i++){
+            if(listadoAsientos[pos][i] == null)
+                disponibles++;
+        }
+        
+        return disponibles;
+    }
+    
+    /**
+     * Función que comprueba si el asiento a comprar está vacío.
+     * @param f La posicion de la ruta en el arreglo.
+     * @param c La posicion del asiento a comprarse
+     * @return Regresa true si el asiento está vacío y false si está lleno o 
+     * hubo un ingreso inválido.
+     */
+    public boolean comprobarAsiento(int f, int c){
+        if (c <= 0){
+            System.out.printf("Error. Ingrese un numero entre 1 y %d.\n", 
+                    listadoAsientos[f].length);
+            return false;
+        }
+        if (listadoAsientos[f][c - 1] == null)
+            return true;       
+        
+        System.out.println("El asiento ya esta ocupado.");
+        return false;
+    }
+    
+    /**
+     * Función que hace todos los procesos
+     * necesarios para vender un ticket.
+     */
+    public void venderTicket(){
+        System.out.print("\nIngrese el numero de vuelo: ");        
+        int n = lea.nextInt();        
+        Ruta rut = buscarRuta(n);
+        if (rut == null)
+            return;            
+        
+        System.out.println("Destino: " + rut.getCiudadDestino());
+        int rutPos = rut.getPosicion();
+        int disp = asientosDisponibles(rutPos);
+        if (disp == 0)
+        {
+            System.out.println("\nNO HAY BOLETOS, AVION LISTO PARA DESPACHAR.\n");
+            return;
+        }
+        System.out.printf("Cantidad de asientos disponibles: %d\n", disp);  
+        
+        String id = rut.createPasajero();
+        elegirAsiento(rut, id);                
+    }
+    
+    /**
+     * Función para elegir el asiento del pasajero
+     * @param rut El objeto Ruta 
+     * @param id El número de identidad (String) del pasajero
+     */
+    public void elegirAsiento(Ruta rut, String id){
+        boolean comp = false;
+        int seat = 0, rutPos = rut.getPosicion();
+        
+        do{
+            System.out.printf("Ingrese un numero entre 1 y %d del asiento que desea comprar: ", 
+                    listadoAsientos[rutPos].length);
+            seat = lea.nextInt();
+            if (seat == -1){
+                System.out.println("CANCELANDO...");
+                break;
+            }                
+            comp = comprobarAsiento(rutPos, seat);
+        }while(!comp);
+        
+        setListadoAsientos(rutPos, seat, id);
+        rut.boletoPasajero(id, seat);
+    }    
+    
+    /**
+     * Ingresa la identidad del pasajero en el asiento.
+     * @param v Número del vuelo
+     * @param p Número del asiento
+     * @param i Número de identidad
+     */
+    public void setListadoAsientos(int v, int p, String i){
+        listadoAsientos[v][p - 1] = i;
+    }    
+
+    public void despacharVuelo() {
+        System.out.println("Ingrese el numero de vuelo: ");
+        int n = lea.nextInt();
+        
+        Ruta rut = buscarRuta(n);
+        printDespacho(rut);
+        if (rut == null)
+            return;
+           
+    }
+    
+    public void printDespacho(Ruta rut){
+        System.out.printf("\nDespachando la ruta %d hacia %s\n" +
+                "Cantidad de asientos comprados: %d\n" + 
+                "Cantidad de primera clase: %d\n" +
+                "Cantidad de clase economica: %d\n" +
+                "Monto total generado: %.2f");             
     }
 }
--- TransporteAerea/src/Aerolinea/Ruta.java
+++ TransporteAerea/src/Aerolinea/Ruta.java
@@ -3,242 +3,166 @@ package Aerolinea;
 import java.util.Scanner;
 
 public class Ruta {
-    private static String ident;
-    private static int asiento;
-    private static Pasajero rentable;
-    private static double ventas;
-    private static double ganancias;
-    Pasajero pasajeros[];
-    Scanner rd = new Scanner(System.in);
-    private int maxi;
-    private int id, clase1, llenos, fila;
-    private String destino;
-    private double precio1, precioe, costo;
+    private int numVuelo, posicion;
+    private String ciudadDestino;
+    private int cantAsientos, cantPrimeraClase;
+    private double precioPrimeraClase, precioClaseEconomica;
+    private double costoDespacho;
+    private static int RUTAS_CREADAS;
+    private Pasajero [] personas;
     
-       
-    /**
-     * Constructor de Ruta
-     * @param fila Fila del arreglo unidimensional de rutas donde se esta creando la ruta
-     */
-    public Ruta(int fila){
-        this.fila = fila;
-        System.out.print("Ingrese numero de Vuelo: "); id = rd.nextInt();
-        System.out.print("Ingrese Destino: "); destino = rd.next();
-        System.out.print("Ingrese Cantidad de Asientos: "); maxi = rd.nextInt();
-        pasajeros = new Pasajero[maxi];
-        System.out.print("Ingrese cantidad de asientos de primera clase: "); clase1 = rd.nextInt();
-        System.out.print("Ingrese precio de asiento en primera clase: ");precio1 = rd.nextInt();
-        System.out.print("Ingrese precio de asiento economico: ");precioe = rd.nextInt();
-        System.out.print("Ingrese costo de despacho de avion: ");costo = rd.nextInt();
-        System.out.print("<<----- RUTA CREADA ----->>");
-    }
-    
-    public int getMaxiAsientos(){
-        return maxi;
-    }
-    
-    public static String getIdCreado(){
-        return ident;
-    }
-    
-    public static int getAsientoCreado(){
-        return asiento;
-    }
+    private static Scanner lea = new Scanner(System.in);
     
     /**
-     * Funcion para agregar nuevo pasajero a la ruta
-     * @return Devuelve true si se creo el pasajero o false en caso contrario.
+     * Constructor de rutas
+     * @param num Número de vuelo de la ruta
+     * @param ciudad Ciudad de destino de la ruta
+     * @param asientos Cantidad de asientos de la ruta
+     * @param primera Cantidad de asientos de primera clase
+     * @param precP Precio de asientos de primera clase
+     * @param precE Precio de asientos de clase económica
+     * @param costo Costo de despachar el vuelo
+     * @param pos Posición de la ruta en el arreglo de rutas
      */
-    public boolean addPasajeroToRuta(){
-        String nombre, id;
-        char genero;
-        int edad, vuelo, numero;
-        boolean state=false;
+    public Ruta(int num, String ciudad, int asientos, 
+        int primera,  double precP, double precE, double costo, int pos){
+        numVuelo = num;
+        ciudadDestino = ciudad;
+        cantAsientos = asientos;
+        cantPrimeraClase = primera;
+        precioPrimeraClase = precP;
+        precioClaseEconomica = precE;
+        costoDespacho = costo;
+        posicion = pos;
+        RUTAS_CREADAS++;
+
+        personas = new Pasajero[asientos];
+        System.out.println("\nRUTA CREADA!");
+
+    } 
+
+    public int getNumVuelo(){
+        return numVuelo;
+    }
+
+    public String getCiudadDestino(){
+        return ciudadDestino;
+    }
+
+    public int getCantAsientos(){
+        return cantAsientos;
+    }
+
+    public int getCantPrimeraClase(){
+        return cantPrimeraClase;
+    }
+
+    public double getPrecioPrimeraClase(){
+        return precioPrimeraClase;
+    }
+
+    public double getPrecioClaseEconomica(){
+        return precioClaseEconomica;
+    }
+
+    public double getCostoDespacho(){
+        return costoDespacho;
+    }
+    
+    public int getPosicion(){
+        return posicion;
+    }
+
+    /**
+     * Imprime la información detallada de la ruta.
+     */
+    public void print(){
+        System.out.printf("Numero de Vuelo: %d - Ciudad de destino: %s - Cantidad de asientos: %d " + 
+            "\nCantidad de asientos de primera clase: %d - Precio Asiento de Primera Clase: %d - "+
+            "\nPrecio Asiento de Clase Economica: % d - Costo de Despacho del Vuelo: % d", numVuelo, ciudadDestino,
+            cantAsientos, cantPrimeraClase, precioPrimeraClase, precioClaseEconomica, costoDespacho);
+    }
+    
+    public double calcularPrecioAsiento(int asiento){
+        if (asiento > 0 && asiento <= getCantPrimeraClase()){
+            return getPrecioPrimeraClase();
+        }
+        return getPrecioClaseEconomica();
+    }
+   
+    /**
+     * Función para buscar la próxima posición en
+     * el arreglo de Pasajeros
+     * @return La posicion i del arreglo de pasajeros.
+     */
+    public int nextPasaPos(){
+        for(int i = 0; i < cantAsientos; i++){
+            if (personas[i] == null)
+                return i;
+        }
+        return -1;
+    }
+    
+    /**
+     * Crea el nuevo pasajero de la ruta.
+     * @return La identidad del pasajero
+     */
+    public String createPasajero(){
+        int esp = nextPasaPos();
         
-        System.out.print("Ingrese numero de identidad: "); id = rd.next(); ident=id;
-        System.out.print("ingrese Nombre Completo: "); nombre = rd.next();
-        System.out.print("Ingrese edad: "); edad = rd.nextInt();
-        System.out.print("Ingrese genero: "); genero = rd.next().charAt(0);
-        do{
-            System.out.printf("Ingrese Numero de Asiento entre 1 y %d: ", maxi); numero = rd.nextInt();
-            if (numero <=getMaxAsientos() && numero!=0){
-                if (pasajeros[numero-1]==null){
-                    pasajeros[numero-1] = new Pasajero(id, nombre, edad, genero, fila);
-                    asiento = numero - 1;
-                    if (numero>0 && numero<=clase1){
-                        pasajeros[numero-1].calcularTotal(precio1, precio1, precioe);
-                    }else{
-                        pasajeros[numero-1].calcularTotal(precioe, precio1, precioe);
-                    }
-                    System.out.print("-->>Imprimiendo facturacion de boleto:\n");
-                    pasajeros[numero-1].printTotales();
-                    ventas+=pasajeros[numero-1].getTotal();
-                    llenos++;
-                    state = true;
-                }else{
-                    System.out.print("Asiento no disponible, Intentelo de Nuevo");
-                }
-            }else if(numero==-1){
-                System.out.print("Accion Cancelada");
-                break;
-            }else
-                System.out.print("Numero de Asiento no Existe, Intentelo de Nuevo");
-        }while (state = false);
-        System.out.print("<<----- PASAJERO CREADO ----->>");
-        return state;
-    }
-    
-    public void deletePasajero(){
-        int numero, cont=0;
-        String conf, id;
-        int state=0;
-        System.out.print("Ingrese Numero de Identidad: "); id = rd.next();
-        System.out.print("Ingrese Numero de Asiento: "); numero = rd.nextInt();
-        if (numero <=getMaxAsientos() && numero!=0){
-            for(Pasajero p : pasajeros){
-                if (p.getId().equalsIgnoreCase(id) && cont==numero-1){
-                    ventas-=p.getTotal();
-                    System.out.print("Realmente desea eliminar su Ticket(si/no)?"); conf = rd.next();
-                    if (conf.equalsIgnoreCase("si")){
-                        p = null;
-                        System.out.print("Pasajero Eliminado Correctamente");
-                        state = 1;
-                    }else
-                        System.out.print("Accion Cancelada\n");
-                    break;
-                }else
-                    state = 2;
-                cont++;
+        if (esp == -1)
+            return null;
+        
+        System.out.print("Ingrese su numero de identidad: ");
+        String ident = lea.next();
+        System.out.print("Ingrese su primer nombre: ");
+        String nomb = lea.next();
+        System.out.print("Ingrese su apellido: ");
+        nomb += " " + lea.next();
+        System.out.print("Ingrese su edad: ");
+        int ed = lea.nextInt();
+        System.out.print("Ingrese su genero: ");
+        String gen = lea.next();
+        
+        personas[esp] = new Pasajero(ident, numVuelo, ed, nomb, gen);
+        
+        //double sub = calcularPrecioAsiento(asiento);
+        //personas[esp].setTotales(sub, getPrecioClaseEconomica(), getPrecioPrimeraClase());
+        
+        return ident;
+    } 
+         
+    /**
+     * Busca si el pasajero existe en la ruta a base de su identidad
+     * @param id El número de identidad del pasajero a buscar.
+     * @return El objeto Pasajero encontrado; null si no existe.
+     */
+    public Pasajero buscarPasajero(String id){
+        for (Pasajero pas : personas){
+            if (pas != null && pas.getId().equals(id)){
+                System.out.printf("El pasajero ha sido creado.\n");
+                return pas;            
             }
         }
-        if (state==2)
-            System.out.print("No existe Pasajero con dicho numero de identidad");
-        
+        System.out.printf("El pasajero no existe.");
+        return null;
+    }    
+    
+    public void boletoPasajero(String id, int seat){
+        Pasajero pasa = buscarPasajero(id);
+        double subTotal = calcularPrecioAsiento(seat);
+        pasa.setTotales(subTotal, getPrecioClaseEconomica(), getPrecioPrimeraClase());
+        pasa.printBoleto();
     }
     
-    /**
-     * Funcion para obtener la cantidad de asientos disponibles
-     * @return Devuelve el numero de asientos vacios
-     */
-    public int cantAsientos(){
-        int cant=0;
-        for(Pasajero p : pasajeros){
-            if (p==null)
-                cant++;
+    public void borrarPasajeros(){
+        for (Pasajero p : personas){
+            p = null;
         }
-        if (cant>0)
-            System.out.print("\tAsientos disponibles: "+cant);
-        else
-            System.out.print("\tNo hay asientos disponibles, Avion listo para despachar");
-        return cant;
     }
     
-    /**
-     * Funcion para obtener el destino de la ruta en la que se encuentra
-     * @return Devuelve el destino de la ruta.
-     */
-    public String getDestino(){
-        return destino;
+    public static int getRutasCreadas(){
+        return RUTAS_CREADAS;
     }
     
-    /**
-     * Funcion para obtener el numero de vuelo de la ruta en que se encuentra
-     * @return Devuelve el numero de vuelo que representa a la ruta
-     */
-    public int getNumeroVuelo(){
-        return id;
-    }
-    
-    /**
-     * Funcion para obtener la cantidad maxima de asientos de la ruta
-     * @return Devuelve la cantidad maxima de asientos que se pueden crear en la ruta
-     */
-    public int getMaxAsientos(){
-        return maxi;
-    }
-    
-    /**
-     * Funcion para obtenes la cantidad de asientos de primera clase
-     * @return Devuelve la cantidad de asientos de primera clase
-     */
-    public int getPrimeraClase(){
-        return clase1;
-    }
-    
-    /**
-     * Funcion para obtener el precio del asiento tomando el cuenta la clase donde se encuentra el asiento
-     * @param x Numero de asiento
-     * @return Devuelve el precio del asiento
-     */
-    public double getPrecio(int x){
-        if (x<=clase1)
-            return precio1;
-        return precioe;
-    }
-    
-    /**
-     * Funcion para validar que el asiento x exista en la ruta
-     * @param x Numero de asiento a validar.
-     * @return Devuelve true si el asiento existe o false caso contrario
-     */
-    public boolean validarNumeroAsientoExista(int x){
-        if (x<=maxi)
-            return true;
-        return false;
-    }
-    
-    /**
-     * Funcion para validar que el asiento x esta vacio
-     * @param x Numero de asiento a validar
-     * @return Devuelve true si el asiento esta vacio o false en caso contrario
-     */
-    public boolean validarAsientoVacio(int x){
-        if (pasajeros[x] == null)
-            return true;
-        return false;
-    }
-    
-    /**
-     * Funcion para devolver las ganancias de la ruta
-     * @return Devuelve las ganancias percibidas
-     */
-    public double getGanancias(){
-        return ganancias;
-    }
-    
-    /**
-     * Funcion para obtener el Numero de Vuelo
-     * @return Devuelve el numero de vuelo
-     */
-    public int getId(){
-        return id;
-    }
-    
-    /**
-     * Funcion para obtener la fila
-     * @return Devuelve la fila
-     */
-    public int getFila(){
-        return fila;
-    }
 }
-
-/* Codigos Eliminados
-    public void add(int i, int c, int l, String d, int p, int cs){
-        id = i;
-        clase1 = c;
-        llenos = 0;
-        destino = d;
-        precio = p;
-        costo = cs;
-    }
-
-public boolean creada(int i){
-        if (id==i)
-            return true;
-        return false;
-    }
-    public double getPrecio(){
-        return precio;
-    }
-*/
