e6465602ef2ad01b0a90903140c63c1b72fb4440
Raim Interiano <rai.interiano@gmail.com>
2014-09-17 18:32:13 -0600

    Final?
    Menu cambiado.

--- TransporteAerea/src/Aerolinea/TransporteAerea.java
+++ TransporteAerea/src/Aerolinea/TransporteAerea.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 public class TransporteAerea {
    
    public static void main(String[] args) {
-   	Scanner lea = new Scanner(System.in);
-   	int res = 0, cant = 0;
-   	Aerolinea aero;
+    Scanner lea = new Scanner(System.in);
+    int res = 0, cant = 0;
+    Aerolinea aero;
 
-   	do{
-            System.out.print("Ingrese la cantidad MAXIMA de rutas que manejara la aerolinea: ");
-            cant = lea.nextInt();
-   	}while(cant <= 0);
-	   	
-	aero = new Aerolinea(cant);//CREACION DE LA AEROLINEA  	 
-	
-        do{//LOGIN	   		
+    do{
+        System.out.print("Ingrese la cantidad MAXIMA de rutas que manejara la aerolinea: ");
+        cant = lea.nextInt();
+    }while(cant <= 0);
+        
+    aero = new Aerolinea(cant);//CREACION DE LA AEROLINEA    
+    
+        do{//LOGIN          
             Usuario userAct = aero.login();
-	    char op = 0;//variable de control de SUB-MENUS
+        char op = 0;//variable de control de SUB-MENUS
 
             do{//MENU
                 System.out.print("\n\t--***MENU***--\n1. Crear una ruta.\n2. Boleteria." +
@@ -27,104 +27,109 @@ public class TransporteAerea {
                 
                res = lea.nextInt();
                
-                switch (res){	   				
+                switch (res){                   
                     case 1: 
-                        if (!userAct.getTipoCuenta().equals("ADMIN"))
-                            continue;
-                        aero.createRuta();
-                        break;	   
-                        
-                    case 2:	
+                        aero.createRuta(); 
+                    case 2: 
                         do{
                             System.out.print("\n\t***OPCIONES***\na. Vender ticket." +
                                     "\nb. Cancelar ticket.\nc. Regresar al menú principal." +
                                     "\nIngrese el caracter de su seleccion: ");
                             op = lea.next().charAt(0);
 
-                            switch (Character.toLowerCase(op)){//SUB-MENU BOLETERIA	   				
+                            switch (Character.toLowerCase(op)){//SUB-MENU BOLETERIA                 
                                 case 'a':                             
                                     System.out.println("\n\t***VENDER TICKET***");        
                                     aero.venderTicket();
-                                    break;	   				
-                                case 'b':	
-                                 System.out.println("\n\t***CANCELAR TICKET***"); 
+                                    break;                  
+                                case 'b':   
+                                    System.out.println("\n\t***CANCELAR TICKET***"); 
                                     aero.cancelarTicket();
-                                    break;	   				
+                                    break;                  
                                 case 'c':
                                     System.out.println("\n\t***SALIENDO AL MENU PRINCIPAL***"); 
-                                    break;	   				
+                                    break;                  
                                 default:
                                     System.out.println("OPCION INCORRECTA! REGRESANDO AL MENU...");
-                            }	
-                        }while(op!='c');   				
+                            }   
+                        }while(op!='c');                
                         break;
                         
                     case 3:                                                
                         System.out.println("\n\t***DESPACHAR VUELO***"); 
                         aero.despacharVuelo();
-                        break;	
+                        break;  
                         
                     case 4:
-                        System.out.print("\n\t***OPCIONES***\na. Estadisticas generales." +
-                                "\nb. Estatidisticas por ruta.\nc. Listado de pasajeros." +
-                                "\nd. Mis datos.\ne. Log.\nf. Regresar al menu principal." +
-                                "\nIngrese el caracter de su seleccion: ");
-                        op = lea.next().charAt(0);
+                        do{
+                            System.out.print("\n\t***OPCIONES***\na. Estadisticas generales." +
+                                    "\nb. Estatidisticas por ruta.\nc. Listado de pasajeros." +
+                                    "\nd. Mis datos.\ne. Log.\nf. Regresar al menu principal." +
+                                    "\nIngrese el caracter de su seleccion: ");
+                            op = lea.next().charAt(0);
 
-                        switch (Character.toLowerCase(op)){//SUB-MENU REPORTES	   					
-                            case 'a':
-                                aero.statsGenerales();
-                                break;	   					
-                            case 'b':                                
-                                System.out.println("\n\t***ESTATIDISTCAS POR RUTA***");
-                                aero.printStatsRuta();
-                                break;	   					
-                            case 'c':
-                                System.out.println("\n\t***LISTADO DE PASAJEROS***");
-                                aero.printListadoPasajeros();
-                                break;	   					
-                            case 'd':
-                                System.out.println("\n\t***MIS DATOS***");
-                                userAct.print();
-                                break;	   					
-                            case 'e':	   					
-                                break;	   					
-                            case 'f':                                                                   
-                                System.out.println("\n\t***SALIENDO AL MENU PRINCIPAL***"); 
-                                break;	   				
-                            default:                                                                   
-                                System.out.println("OPCION INCORRECTA! REGRESANDO AL MENU...");
-                        }                    	
-                        break;	
+                            switch (Character.toLowerCase(op)){//SUB-MENU REPORTES                      
+                                case 'a':
+                                    aero.statsGenerales();
+                                    break;                      
+                                case 'b':                                
+                                    System.out.println("\n\t***ESTATIDISTICAS POR RUTA***");
+                                    aero.printStatsRuta();
+                                    break;                      
+                                case 'c':
+                                    System.out.println("\n\t***LISTADO DE PASAJEROS***");
+                                    aero.printListadoPasajeros();
+                                    break;                      
+                                case 'd':
+                                    System.out.println("\n\t***MIS DATOS***");
+                                    userAct.print();
+                                    break;                      
+                                case 'e':                       
+                                    break;                      
+                                case 'f':                                                                   
+                                    System.out.println("\n\t***SALIENDO AL MENU PRINCIPAL***"); 
+                                    break;                  
+                                default:                                                                   
+                                    System.out.println("OPCION INCORRECTA! REGRESANDO AL MENU...");
+                            }             
+                        }while(op!='f');
+                        break;  
                         
-                    case 5:	 
-                        System.out.print("\n\t***OPCIONES***\na. Editar mis datos." +
-                                "\nb. Crear usuario.\nc. Regresar al menu principal." +
-                                "\nIngrese el caracter de su seleccion: ");
-                        op = lea.next().charAt(0);
-                                                  
-                        switch (Character.toLowerCase(op)){//SUB-MENU USUARIOS	   				
-                            case 'a':	   				
-                                break;	   					
-                            case 'b':	   					
-                                break;	   					
-                            case 'c':                                                
-                                System.out.println("\n\t***SALIENDO AL MENU PRINCIPAL***"); 
-                                break;	   					
-                            default:	   					
-                        }	   				
-                        break;	
+                    case 5: 
+                        do{
+                            System.out.print("\n\t***OPCIONES***\na. Editar mis datos." +
+                                    "\nb. Crear usuario.\nc. Regresar al menu principal." +
+                                    "\nIngrese el caracter de su seleccion: ");
+                            op = lea.next().charAt(0);
+
+                            switch (Character.toLowerCase(op)){//SUB-MENU USUARIOS                  
+                                case 'a':   
+                                    System.out.println("\n\t***EDITAR MIS DATOS***"); 
+                                    aero.editarUsuario();
+                                    break;                      
+                                case 'b':     
+                                    System.out.println("\n\t***CREAR USUARIO***"); 
+                                    aero.crearUsuario();
+                                    break;                      
+                                case 'c':      
+                                    System.out.println("\n\t***SALIENDO AL MENU PRINCIPAL***"); 
+                                    break;                      
+                                default:      
+                                    System.out.println("OPCION INCORRECTA! REGRESANDO AL MENU...");
+                            }       
+                        }while (op!='c');
+                        break;  
                         
                     case 6:
                         System.out.println("CERRANDO SESION...");
-                        break;	  
+                        break;    
                         
-                    case 7:	   
+                    case 7:    
                         System.out.println("SALIENDO DEL PROGRAMA...");
                         break;
                         
-                    default:	   				
-                        System.out.println("Opcion incorrecta; ingrese su opcion de nuevo.");	   			
+                    default:                    
+                        System.out.println("Opcion incorrecta; ingrese su opcion de nuevo.");               
                 }   
                 
             }while (res != 6 && res != 7);//repite mientras no se ingrese 6 o 7
